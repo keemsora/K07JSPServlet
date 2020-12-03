f965698e218cd41352bad6eefe098662482c1dbe
Sora <Sora@DESKTOP-985U39B>
2020-12-03 23:08:30 +0900

    K07JSPServlet

--- src/eltag/MyTagLib.java
+++ src/eltag/MyTagLib.java
@@ -1,5 +1,7 @@
 package eltag;
 
+import model.MemberDAO;
+
 /*
 	EL에서 Java클래스의 메소드 호출 절차 및 방법
 	1. 클래스와 메소드를 정의한다. 단 메소드 정의 시 반드시 
@@ -47,6 +49,20 @@ public class MyTagLib {
 		return returnStr;
 	}
 	
+	/*
+	아이디/패스워드, DB연결을 위한 드라이버명, URL을 인자로 전달받아
+	회원여부를 판단하여 boolean을 반환해주는 메소드 
+	*/
+	public static boolean memberLogin(String id, String pw, 
+			String drv, String url) {
+		//DB연결을 위한 객체생성
+		MemberDAO dao = new MemberDAO(drv, url);
+		//아이디, 패스워드를 통한 회원인증 및 결과반환
+		boolean isBool = dao.isMember(id, pw);
+		//위 결과를 호출한 지점으로 반환
+		return isBool;
+	}
+	
 	//메소드 테스트를 위한 main메소드
 	public static void main(String[] args) {
 		boolean result1 = isNumber("1234");
@@ -54,4 +70,6 @@ public class MyTagLib {
 		System.out.println("결과1:"+result1);
 		System.out.println("결과2:"+result2);
 	}
+	
+	
 }
